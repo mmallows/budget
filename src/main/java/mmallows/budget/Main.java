8d0ddc4eed538afc9c002c611319e3c7e532dec9
Marcus Mallows <marcusmallows@aol.com>
2026-03-10 22:05:51 -0400

    Update Main to use input file path instead of hardcoded & create/connect to DB

--- src/main/java/mmallows/budget/Main.java
+++ src/main/java/mmallows/budget/Main.java
@@ -13,12 +13,27 @@ import mmallows.budget.Utils.TransactionImporter;
 public class Main {
     public static void main(String[] args) {
 
+        String url = "jdbc:sqlite:budget.db";
+
+        try (Connection conn = DriverManager.getConnection(url)) {
+            if (conn != null) {
+                System.out.println("Connected to SQLite database!");
+
+                // TODO: Write a more comprehensive database initialization routine
+                // To iterate over all my classes and create all my tables
+
+            }
+        } catch (SQLException e) {
+            System.out.println("Database connection failed.");
+            e.printStackTrace();
+        }
+
         System.out.println("Welcome to the mmallows Budget App!");
         System.out.println("Please input a file path to import CSV data:");
-        String filePath = "C:/Users/Marcus/Downloads/transactions (3).csv";
-        // try (Scanner input = new Scanner(System.in)) {
-        // filePath = input.nextLine();
-        // }
+        String filePath = "";
+        try (Scanner input = new Scanner(System.in)) {
+            filePath = input.nextLine();
+        }
 
         TransactionImporter importer = new TransactionImporter(filePath);
         HashMap<String, Double> summary = importer.getTransactionTotalSummary();
@@ -28,25 +43,5 @@ public class Main {
         for (String category : summary.keySet()) {
             System.out.printf("%-20s : $%.2f%n", category, summary.get(category));
         }
-
-        // String url = "jdbc:sqlite:budget.db"; // This will create budget.db in your
-        // project folder
-
-        // try (Connection conn = DriverManager.getConnection(url)) {
-        // if (conn != null) {
-        // System.out.println("Connected to SQLite database!");
-
-        // // TODO: Write a more comprehensive database initialization routine
-        // // To iterate over all my classes and create all my tables
-
-        // try (Statement stmt = conn.createStatement()) {
-        // stmt.execute(sql);
-        // System.out.println("Table 'transaction' is ready to use.");
-        // }
-        // }
-        // } catch (SQLException e) {
-        // System.out.println("Database connection failed.");
-        // e.printStackTrace();
-        // }
     }
 }
